67d8ae64b9ab79e4142a51faa0ed3bca6ae732bb
shobhitr97 <shobhitr97@gmail.com>
2016-06-17 17:52:10 +0530

    Update BadNeighbors.cpp

--- BadNeighbors.cpp
+++ BadNeighbors.cpp
@@ -6,4 +6,19 @@
 using namespace std;
 
 class BadNeighbors{
+	public:
+	int max(int a,int b){
+		if(a>b){
+			return a;
+		}
+		return b;
+	}
+	int maxDonations(vector <int> amt){
+		vector <int> a(don.size());
+		a[0]=amt[0];
+		a[1]=amt[1]+amt[amt.size()-1];
+		for(i=2;i<amt.size()-2;i++){
+			a[i]=max((a[i-2]+amt[i]),(a[i-1]+amt[i]-amt[i-1]));
+		}
+	}//need to be edited
 }
