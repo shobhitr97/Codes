5b5beae6eae5999521d35ebf4bef347d53102378
shobhitr97 <shobhitr97@gmail.com>
2016-06-17 17:24:24 +0530

    Update ZigZag.cpp

--- ZigZag.cpp
+++ ZigZag.cpp
@@ -2,24 +2,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-uisng namespace std;
-class ZigZag(){
-	int diff[50],S[50],temp,i,j,max;
+using namespace std;
+class ZigZag{
+	int diff[50],S[50],temp,i,j,max,start;
 	public:
 	int longestZigZag(int *seq,int n){
 		for(i=0;i<n;i++){
-			diff[i]=0;
-			S[i]=1;
+		    diff[i]=0;
+		    S[i]=1;
 		}
-		diff[0]=0;
-		diff[1]=seq[1]-seq[0];
-		S[0]=0;
-		S[1]=2;
+		
+		
+	    i=1;
+	    while(seq[i]==seq[i-1]){
+	        i++;
+	    }
+	    diff[i]=seq[i]-seq[i-1];
+	    start=i;
+		S[start]=2;
 		max=0;
-		for(i=2;i<n;i++){
+		i++;
+		
+		while(i<n){
 			temp=0;
 			j=i-1;
-			while(j>=1){
+			while(j>=start){
 				if((seq[i]-seq[j])*diff[j]<0){
 					if(S[j]>S[i]){
 						S[i]=S[j];
@@ -32,16 +39,20 @@ class ZigZag(){
 			if(S[i]>max){
 				max=S[i];
 			}
+			//cout<<S[i]<<"\n";
+			i++;
 		}
+		
 		return max;
 	}
 	
-}
+};
+
 int main(){
 	int A[51],n,i;
 	cin>>n;
 	for(i=0;i<n;i++){
-		cin>>A[i];
+		scanf("%d, ",&A[i]);
 	}
 	ZigZag obj;
 	cout<<obj.longestZigZag(A,n)<<"\n";
